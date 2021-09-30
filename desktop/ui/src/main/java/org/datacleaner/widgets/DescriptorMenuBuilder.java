b00ebeec11ccff5616bd9aaf499af6e77da620db
sathish <cibugs8@gmail.com>
2021-09-30 10:10:09 +0530

    Buggy from https://github.com/datacleaner/DataCleaner/commit/e2f9fd51b0fb

---
+++ desktop/ui/src/main/java/org/datacleaner/widgets/DescriptorMenuBuilder.java
@@ -0,0 +1,209 @@
+/**
+ * DataCleaner (community edition)
+ * Copyright (C) 2014 Neopost - Customer Information Management
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.datacleaner.widgets;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.swing.JComponent;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+
+import org.apache.metamodel.util.CollectionUtils;
+import org.apache.metamodel.util.ImmutableRef;
+import org.apache.metamodel.util.Ref;
+import org.datacleaner.api.ComponentCategory;
+import org.datacleaner.api.ComponentSuperCategory;
+import org.datacleaner.descriptors.ComponentDescriptor;
+import org.datacleaner.descriptors.DescriptorProvider;
+import org.datacleaner.job.builder.AnalysisJobBuilder;
+import org.datacleaner.user.UsageLogger;
+import org.datacleaner.util.CollectionUtils2;
+import org.datacleaner.util.DeprecatedComponentPredicate;
+import org.datacleaner.util.DisplayNameComparator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Builder object that can build menus with {@link ComponentDescriptor} items in
+ * it. Click a {@link ComponentDescriptor} will add it to the job
+ */
+public final class DescriptorMenuBuilder {
+
+    private static final Logger logger = LoggerFactory.getLogger(DescriptorMenuBuilder.class);
+
+    private final AnalysisJobBuilder _analysisJobBuilder;
+    private final UsageLogger _usageLogger;
+    private final Ref<Collection<? extends ComponentDescriptor<?>>> _componentDescriptorsRef;
+    private final boolean _buildSubmenus;
+    private final Point2D _coordinate;
+
+    public DescriptorMenuBuilder(final AnalysisJobBuilder analysisJobBuilder, final UsageLogger usageLogger,
+            final Collection<? extends ComponentDescriptor<?>> descriptors, final Point2D coordinate) {
+        this(analysisJobBuilder, usageLogger, descriptors, coordinate, true);
+    }
+
+    public DescriptorMenuBuilder(final AnalysisJobBuilder analysisJobBuilder, final UsageLogger usageLogger,
+            final ComponentSuperCategory superCategory, final Point2D coordinate) {
+        this(analysisJobBuilder, usageLogger, superCategory, coordinate, true);
+    }
+
+    public DescriptorMenuBuilder(final AnalysisJobBuilder analysisJobBuilder, final UsageLogger usageLogger,
+            final Collection<? extends ComponentDescriptor<?>> descriptors, final Point2D coordinate,
+            final boolean buildSubmenus) {
+        final Collection<? extends ComponentDescriptor<?>> filteredDescriptors = CollectionUtils.filter(descriptors,
+                new DeprecatedComponentPredicate());
+        final List<ComponentDescriptor<?>> componentDescriptors = new ArrayList<>(filteredDescriptors);
+        Collections.sort(componentDescriptors, new DisplayNameComparator());
+
+        _analysisJobBuilder = analysisJobBuilder;
+        _usageLogger = usageLogger;
+        _coordinate = coordinate;
+        _buildSubmenus = buildSubmenus;
+        _componentDescriptorsRef = new ImmutableRef<Collection<? extends ComponentDescriptor<?>>>(componentDescriptors);
+    }
+
+    public DescriptorMenuBuilder(final AnalysisJobBuilder analysisJobBuilder, final UsageLogger usageLogger,
+            final ComponentSuperCategory superCategory, final Point2D coordinate, final boolean buildSubmenus) {
+        _analysisJobBuilder = analysisJobBuilder;
+        _usageLogger = usageLogger;
+        _coordinate = coordinate;
+        _buildSubmenus = buildSubmenus;
+        _componentDescriptorsRef = new Ref<Collection<? extends ComponentDescriptor<?>>>() {
+            @Override
+            public Collection<? extends ComponentDescriptor<?>> get() {
+                final DescriptorProvider descriptorProvider = analysisJobBuilder.getConfiguration()
+                        .getDescriptorProvider();
+                final Collection<? extends ComponentDescriptor<?>> componentDescriptors = descriptorProvider
+                        .getComponentDescriptorsOfSuperCategory(superCategory);
+                return componentDescriptors;
+            }
+        };
+    }
+
+    public void addItemsToMenu(JMenu menu) {
+        initialize(menu);
+    }
+
+    public void addItemsToPopupMenu(JPopupMenu menu) {
+        initialize(menu);
+    }
+
+    private void initialize(final JComponent outerMenu) {
+        final Collection<? extends ComponentDescriptor<?>> unsortedComponentDescriptors = _componentDescriptorsRef
+                .get();
+        final List<? extends ComponentDescriptor<?>> componentDescriptors = CollectionUtils2
+                .sorted(unsortedComponentDescriptors);
+
+        if (!_buildSubmenus) {
+            for (ComponentDescriptor<?> descriptor : componentDescriptors) {
+                final JMenuItem menuItem = createMenuItem(descriptor);
+                outerMenu.add(menuItem);
+            }
+            return;
+        }
+
+        final Map<ComponentCategory, DescriptorMenu> descriptorMenus = new HashMap<ComponentCategory, DescriptorMenu>();
+
+        // build sub menus
+        {
+            for (ComponentDescriptor<?> descriptor : componentDescriptors) {
+                final Set<ComponentCategory> componentCategories = descriptor.getComponentCategories();
+                for (ComponentCategory componentCategory : componentCategories) {
+                    DescriptorMenu menu = descriptorMenus.get(componentCategory);
+                    if (menu == null) {
+                        menu = new DescriptorMenu(componentCategory);
+                        descriptorMenus.put(componentCategory, menu);
+                    }
+                    menu.addComponentClass(descriptor.getComponentClass());
+                }
+            }
+        }
+
+        {
+            // place sub menus
+            final List<DescriptorMenu> sortedMenus = CollectionUtils2.sorted(descriptorMenus.values());
+            for (DescriptorMenu descriptorMenu : sortedMenus) {
+                final int count = descriptorMenu.getComponentClassCount();
+                if (count <= 1) {
+                    // disregard categories with only a single component in
+                    // them!
+                    ComponentCategory category = descriptorMenu.getComponentCategory();
+                    logger.info("Disregarding menu for category '{}' because of too few components ({})", category,
+                            count);
+                    descriptorMenus.remove(category);
+                } else {
+                    // add menu
+                    outerMenu.add(descriptorMenu);
+                }
+            }
+        }
+
+        // place items that are not in any submenus
+        {
+            final List<? extends ComponentDescriptor<?>> sortedComponentDescriptors = CollectionUtils2
+                    .sorted(componentDescriptors);
+            for (final ComponentDescriptor<?> descriptor : sortedComponentDescriptors) {
+                boolean placedInSubmenu = false;
+                final Class<?> componentClass = descriptor.getComponentClass();
+                JMenuItem menuItem = createMenuItem(descriptor);
+                if (menuItem != null) {
+                    List<DescriptorMenu> sortedMenusDescriptors = CollectionUtils2.sorted(descriptorMenus.values());
+                    for (DescriptorMenu descriptorMenu : sortedMenusDescriptors) {
+                        if (descriptorMenu.containsComponentClass(componentClass)) {
+                            descriptorMenu.add(menuItem);
+                            placedInSubmenu = true;
+
+                            // create a new menu item (or else it will be moved
+                            // instead of added.
+                            menuItem = createMenuItem(descriptor);
+                        }
+                    }
+
+                    if (!placedInSubmenu) {
+                        // add menu item
+                        outerMenu.add(menuItem);
+                    }
+                }
+            }
+        }
+    }
+
+    private JMenuItem createMenuItem(final ComponentDescriptor<?> descriptor) {
+        final DescriptorMenuItem menuItem = new DescriptorMenuItem(_analysisJobBuilder, _coordinate, descriptor);
+        menuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                _usageLogger.logComponentUsage(descriptor);
+            }
+        });
+        return menuItem;
+    }
+
+}
---
+++ desktop/ui/src/main/java/org/datacleaner/widgets/visualization/JobGraphMouseListener.java
@@ -0,0 +1,312 @@
+/**
+ * DataCleaner (community edition)
+ * Copyright (C) 2014 Neopost - Customer Information Management
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.datacleaner.widgets.visualization;
+
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.swing.Icon;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+
+import org.apache.metamodel.schema.Table;
+import org.datacleaner.actions.PreviewSourceDataActionListener;
+import org.datacleaner.actions.PreviewTransformedDataActionListener;
+import org.datacleaner.actions.RemoveComponentMenuItem;
+import org.datacleaner.actions.RemoveSourceTableMenuItem;
+import org.datacleaner.actions.RenameComponentActionListener;
+import org.datacleaner.api.ComponentSuperCategory;
+import org.datacleaner.bootstrap.WindowContext;
+import org.datacleaner.configuration.AnalyzerBeansConfiguration;
+import org.datacleaner.connection.Datastore;
+import org.datacleaner.data.MetaModelInputColumn;
+import org.datacleaner.job.HasFilterOutcomes;
+import org.datacleaner.job.InputColumnSourceJob;
+import org.datacleaner.job.builder.AnalysisJobBuilder;
+import org.datacleaner.job.builder.ComponentBuilder;
+import org.datacleaner.job.builder.TransformerComponentBuilder;
+import org.datacleaner.metadata.HasMetadataProperties;
+import org.datacleaner.user.UsageLogger;
+import org.datacleaner.util.IconUtils;
+import org.datacleaner.util.ImageManager;
+import org.datacleaner.util.WidgetFactory;
+import org.datacleaner.widgets.ChangeRequirementMenu;
+import org.datacleaner.widgets.DescriptorMenuBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.uci.ics.jung.algorithms.layout.AbstractLayout;
+import edu.uci.ics.jung.visualization.control.GraphMouseListener;
+import edu.uci.ics.jung.visualization.picking.PickedState;
+
+/**
+ * Listener for mouse events on the {@link JobGraph}.
+ * 
+ * Note that this class implements two interfaces and is thus added as two
+ * distinct listener types: {@link MouseListener} and {@link GraphMouseListener}
+ */
+public class JobGraphMouseListener extends MouseAdapter implements GraphMouseListener<Object> {
+
+    private static final Logger logger = LoggerFactory.getLogger(JobGraphMouseListener.class);
+
+
+    private final JobGraphContext _graphContext;
+    private final JobGraphLinkPainter _linkPainter;
+    private final JobGraphActions _actions;
+    private final WindowContext _windowContext;
+    private final UsageLogger _usageLogger;
+
+    // this is ugly, but a hack to make the graph mouse listener and the
+    // regular mouse listener aware of each other's actions.
+    private boolean _clickCaught = false;
+
+    private Point _pressedPoint;
+
+    public JobGraphMouseListener(JobGraphContext graphContext, JobGraphLinkPainter linkPainter, JobGraphActions actions,
+       WindowContext windowContext, UsageLogger usageLogger) {
+        _graphContext = graphContext;
+        _linkPainter = linkPainter;
+        _actions = actions;
+        _windowContext = windowContext;
+        _usageLogger = usageLogger;
+    }
+
+    /**
+     * Invoked when a component is double-clicked
+     * 
+     * @param componentBuilder
+     * @param me
+     */
+    public void onComponentDoubleClicked(ComponentBuilder componentBuilder, MouseEvent me) {
+        _actions.showConfigurationDialog(componentBuilder);
+    }
+
+    /**
+     * Invoked when a {@link Table} is double clicked
+     * 
+     * @param table
+     * @param me
+     */
+    public void onTableDoubleClicked(final Table table, MouseEvent me) {
+        _actions.showTableConfigurationDialog(table);
+    }
+
+    /**
+     * Invoked when a {@link Table} is right-clicked
+     * 
+     * @param table
+     * @param me
+     */
+    public void onTableRightClicked(Table table, MouseEvent me) {
+        final JPopupMenu popup = new JPopupMenu();
+
+        popup.add(createLinkMenuItem(table));
+
+        final JMenuItem previewMenuItem = new JMenuItem("Preview data", ImageManager.get().getImageIcon(
+                IconUtils.ACTION_PREVIEW, IconUtils.ICON_SIZE_SMALL));
+        final AnalysisJobBuilder analysisJobBuilder = _graphContext.getAnalysisJobBuilder();
+        final Datastore datastore = analysisJobBuilder.getDatastore();
+        final List<MetaModelInputColumn> inputColumns = analysisJobBuilder.getSourceColumnsOfTable(table);
+        previewMenuItem.addActionListener(new PreviewSourceDataActionListener(_windowContext, datastore, inputColumns));
+        popup.add(previewMenuItem);
+        popup.addSeparator();
+        popup.add(new RemoveSourceTableMenuItem(analysisJobBuilder, table));
+        popup.show(_graphContext.getVisualizationViewer(), me.getX(), me.getY());
+    }
+
+    /**
+     * Invoked when a component is right-clicked
+     * 
+     * @param componentBuilder
+     * @param me
+     */
+    public void onComponentRightClicked(final ComponentBuilder componentBuilder, final MouseEvent me) {
+        final JPopupMenu popup = new JPopupMenu();
+
+        final JMenuItem configureComponentMenuItem = new JMenuItem("Configure ...", ImageManager.get().getImageIcon(
+                IconUtils.MENU_OPTIONS, IconUtils.ICON_SIZE_SMALL));
+        configureComponentMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                _actions.showConfigurationDialog(componentBuilder);
+            }
+        });
+        popup.add(configureComponentMenuItem);
+
+        if (componentBuilder instanceof InputColumnSourceJob || componentBuilder instanceof HasFilterOutcomes) {
+            popup.add(createLinkMenuItem(componentBuilder));
+        }
+
+        final Icon renameIcon = ImageManager.get().getImageIcon(IconUtils.ACTION_RENAME, IconUtils.ICON_SIZE_SMALL);
+        final JMenuItem renameMenuItem = WidgetFactory.createMenuItem("Rename component", renameIcon);
+        renameMenuItem.addActionListener(new RenameComponentActionListener(componentBuilder) {
+
+            @Override
+            protected void onNameChanged() {
+                _graphContext.getJobGraph().refresh();
+            }
+        });
+        popup.add(renameMenuItem);
+
+        if (componentBuilder instanceof TransformerComponentBuilder) {
+            final TransformerComponentBuilder<?> tjb = (TransformerComponentBuilder<?>) componentBuilder;
+            final JMenuItem previewMenuItem = new JMenuItem("Preview data", ImageManager.get().getImageIcon(
+                    IconUtils.ACTION_PREVIEW, IconUtils.ICON_SIZE_SMALL));
+            previewMenuItem.addActionListener(new PreviewTransformedDataActionListener(_windowContext, tjb));
+            previewMenuItem.setEnabled(componentBuilder.isConfigured());
+            popup.add(previewMenuItem);
+        }
+
+        popup.add(new ChangeRequirementMenu(componentBuilder));
+        popup.addSeparator();
+        popup.add(new RemoveComponentMenuItem(_graphContext.getAnalysisJobBuilder(), componentBuilder));
+        popup.show(_graphContext.getVisualizationViewer(), me.getX(), me.getY());
+    }
+
+    private JMenuItem createLinkMenuItem(final Object from) {
+        final ImageManager imageManager = ImageManager.get();
+        final JMenuItem menuItem = new JMenuItem("Link to ...", imageManager.getImageIcon(IconUtils.ACTION_ADD,
+                IconUtils.ICON_SIZE_SMALL));
+        menuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                _linkPainter.startLink(from);
+            }
+        });
+        return menuItem;
+    }
+
+    /**
+     * Invoked when the canvas is right-clicked
+     * 
+     * @param me
+     */
+    public void onCanvasRightClicked(final MouseEvent me) {
+        _linkPainter.cancelLink();
+
+        final JPopupMenu popup = new JPopupMenu();
+
+        final Point point = me.getPoint();
+        final AnalysisJobBuilder analysisJobBuilder = _graphContext.getAnalysisJobBuilder();
+        final AnalyzerBeansConfiguration configuration = analysisJobBuilder.getConfiguration();
+        final Set<ComponentSuperCategory> superCategories = configuration.getDescriptorProvider()
+                .getComponentSuperCategories();
+        for (ComponentSuperCategory superCategory : superCategories) {
+            final DescriptorMenuBuilder menuBuilder = new DescriptorMenuBuilder(analysisJobBuilder, _usageLogger,
+                    superCategory, point);
+
+            final JMenu menu = new JMenu(superCategory.getName());
+            menu.setIcon(IconUtils.getComponentSuperCategoryIcon(superCategory));
+            menuBuilder.addItemsToMenu(menu);
+            popup.add(menu);
+        }
+
+        popup.show(_graphContext.getVisualizationViewer(), me.getX(), me.getY());
+    }
+
+    @Override
+    public void graphReleased(Object v, MouseEvent me) {
+        if (_pressedPoint != null && _pressedPoint.equals(me.getPoint())) {
+            // avoid updating any coordinates when nothing has been moved
+            return;
+        }
+
+        final PickedState<Object> pickedVertexState = _graphContext.getVisualizationViewer().getPickedVertexState();
+
+        final Object[] selectedObjects = pickedVertexState.getSelectedObjects();
+
+        final AbstractLayout<Object, JobGraphLink> graphLayout = _graphContext.getGraphLayout();
+
+        // update the coordinates metadata of the moved objects.
+
+        for (final Object vertex : selectedObjects) {
+            final Double x = graphLayout.getX(vertex);
+            final Double y = graphLayout.getY(vertex);
+            if (vertex instanceof HasMetadataProperties) {
+                final Map<String, String> metadataProperties = ((HasMetadataProperties) vertex).getMetadataProperties();
+                metadataProperties.put(JobGraphMetadata.METADATA_PROPERTY_COORDINATES_X, "" + x.intValue());
+                metadataProperties.put(JobGraphMetadata.METADATA_PROPERTY_COORDINATES_Y, "" + y.intValue());
+            } else if (vertex instanceof Table) {
+                JobGraphMetadata.setPointForTable(_graphContext.getAnalysisJobBuilder(), (Table) vertex, x, y);
+            }
+        }
+
+        if (selectedObjects.length > 0) {
+            _graphContext.getJobGraph().refresh();
+        }
+    }
+
+    @Override
+    public void graphPressed(Object v, MouseEvent me) {
+        _pressedPoint = me.getPoint();
+    }
+
+    @Override
+    public void graphClicked(Object v, MouseEvent me) {
+        logger.debug("graphClicked({}, {})", v, me);
+        _clickCaught = false;
+        final int button = me.getButton();
+        if (v instanceof ComponentBuilder) {
+            final ComponentBuilder componentBuilder = (ComponentBuilder) v;
+            if (button == MouseEvent.BUTTON2 || button == MouseEvent.BUTTON3) {
+                _clickCaught = true;
+                onComponentRightClicked(componentBuilder, me);
+            } else if (me.getClickCount() == 2) {
+                _clickCaught = true;
+                onComponentDoubleClicked(componentBuilder, me);
+            } else {
+                final boolean ended = _linkPainter.endLink(componentBuilder, me);
+                if (ended) {
+                    me.consume();
+                }
+            }
+        } else if (v instanceof Table) {
+            final Table table = (Table) v;
+            if (button == MouseEvent.BUTTON2 || button == MouseEvent.BUTTON3) {
+                _clickCaught = true;
+                onTableRightClicked(table, me);
+            } else if (me.getClickCount() == 2) {
+                _clickCaught = true;
+                onTableDoubleClicked(table, me);
+            }
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent me) {
+        logger.debug("mouseClicked({}) (clickCaught={})", me, _clickCaught);
+        if (!_clickCaught) {
+            int button = me.getButton();
+            if (button == MouseEvent.BUTTON2 || button == MouseEvent.BUTTON3) {
+                onCanvasRightClicked(me);
+            }
+        }
+        // reset the variable for next time
+        _clickCaught = false;
+    }
+}
