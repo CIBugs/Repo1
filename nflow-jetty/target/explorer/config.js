a638e9198779ed6d93dd0cd4ed425fff7eaf04d9
sathish <cibugs8@gmail.com>
2021-10-05 23:07:45 +0530

    Buggy from https://github.com/NitorCreations/nflow/commit/5fd873e39fa6e8a4820d1dcb904f765c5d496641

---
+++ nflow-jetty/target/explorer/config.js
@@ -0,0 +1,15 @@
+'use strict';
+var Config = function() {
+  this.nflowUrl = 'http://localhost:7500/api/';
+  //this.nflowUrl = 'http://nbank.dynalias.com/nflow-dev/api';
+
+  this.nflowApiDocs = 'http://localhost:7500/doc/';
+  //this.nflowApiDocs = 'http://nbank.dynalias.com/nflow/doc/';
+
+  this.radiator = {
+    // poll period in seconds
+    pollPeriod: 15,
+    // max number of items to keep in memory
+    maxHistorySize: 10000
+  };
+};
